5c0ee1a12d83217ff71acfc0bdcd8445946f15fe
jim <jim.yuan@supermicro.com>
2017-03-23 09:43:57 -0700

    p9dsu-xml: Fix AVSBus Settings for VDD/VCS/VDN.

--- openpower/package/p9dsu-xml/p9dsu.mk
+++ openpower/package/p9dsu-xml/p9dsu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-P9DSU_XML_VERSION ?= 071a88b2a02b2a5d1361511e2af0f11c683dd5de
+P9DSU_XML_VERSION ?= 8e2690fce6b26a631e0fd965cc02ccc612953ced
 P9DSU_XML_SITE_METHOD = git
 P9DSU_XML_SITE = git@172.31.32.149:p9/p9dsu-xml.git
 
