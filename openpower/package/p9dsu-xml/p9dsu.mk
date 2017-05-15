a8d46a6d9013a386a79ce7e46812ff3d161999ea
jim <jim.yuan@supermicro.com>
2017-05-15 13:32:50 -0700

    use static IP for test_git sever.

--- openpower/package/p9dsu-xml/p9dsu.mk
+++ openpower/package/p9dsu-xml/p9dsu.mk
@@ -6,7 +6,7 @@
 
 P9DSU_XML_VERSION ?= 6bcc4fbc2182960c28e93fad63ae2dfe4f32e9f2
 P9DSU_XML_SITE_METHOD = git
-P9DSU_XML_SITE = git@172.31.32.149:p9/p9dsu-xml.git
+P9DSU_XML_SITE = git@172.31.3.164:p9/p9dsu-xml.git
 
 P9DSU_XML_LICENSE = Apache-2.0
 P9DSU_XML_DEPENDENCIES = hostboot-install-images openpower-mrw-install-images common-p8-xml-install-images
